7ab45fa4b072047a57ca9e21e164a5869aa7b954
Rom4ik <romajanazizik@gmail.com>
2024-10-05 16:12:07 +0200

    Исправление ошибок в HistoryManager

--- src/com/yandex/app/service/HistoryManager.java
+++ src/com/yandex/app/service/HistoryManager.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);  // Добавить задачу в историю
+
     void remove(int id);  // Удалить задачу по её ID из истории
+
     List<Task> getHistory();  // Вернуть список задач в порядке просмотра
 }
